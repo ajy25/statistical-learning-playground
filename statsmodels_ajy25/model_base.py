3d61a02a3f1b906820c86c9763343acb459e3c0d
ajy25 <andrew_j_yang@brown.edu>
2023-09-01 23:33:08 -0400

    minor renaming

---
+++ statsmodels_ajy25/model_base.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class ModelBase():
+
+    def __init__(self):
+        pass
+    
+    def forward(self, X: np.ndarray):
+        """
+        f(X)
+        """
+        return self.X
+
+
